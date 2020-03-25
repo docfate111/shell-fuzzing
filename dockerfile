b4db3db3cc63d5b1c0923789d1b74b1c75323ff4
docfate111 <tdwilliamsiv@gmail.com>
2020-03-24 17:03:55 -0700

    more to do soon

---
+++ dockerfile
@@ -0,0 +1,48 @@
+FROM ubuntu
+#1. Write a Vagrantfile that sets up a VM with:
+#   a. random generation script.
+#   b. A bunch of shells. At a minimum:
+RUN "sudo apt install git build-essential libreadline-dev"
+RUN "sudo apt-add-repository ppa:fish-shell/release-3"
+RUN "sudo apt-get update"
+#      - dash
+#      - yash
+#      - ksh
+#      - mksh
+#      - bosh
+#      - zsh
+#      - osh
+RUN "cd ~"
+RUN "git clone https://github.com/oilshell/oil.git"
+RUN "git submodule update --init --recursive"
+RUN "osh=~/oil/bin/osh
+#      - fish
+RUN "apt install git bash dash yash ksh mksh bosh zsh fish"
+#      - bash (3.x, 4.x, and 5.x)
+RUN "git clone https://github.com/MacMiniVault/bash3.2.53.git"
+RUN "bash3='/bash3.2.53/bin/bash'"
+#bash 4.4
+RUN "git clone https://github.com/gitGNU/gnu_bash.git"
+RUN "cd gnu_bash"
+RUN "./configure"
+RUN "make"
+RUN "cd .."
+#bash 5.0
+RUN "git clone https://github.com/errodriguez/bash5.git"
+RUN "cd bash5"
+RUN "./configure"
+RUN "make"
+RUN "cd .."
+#      - heirloom SH 
+RUN "git clone https://github.com/Sunshine-OS/heirloom-sh.git"
+RUN "cd heirloom-sh"
+RUN "make"
+RUN "make install"
+RUN "cd -"
+
+# 2. Write a script in the language of your choosing that:
+#   a. Reads in a sample script.
+#   b. Runs each shell on that script, recording STDOUT, STDERR, exit
+#      status, and any changes to the filesystem.
+#   c. Outputs a summary comparing the behavior of each shell, grouping
+#      them into equivalence classes.
---
+++ src/posix/weightedOr.py
@@ -0,0 +1,74 @@
+from gramfuzz.fields import *
+from gramfuzz.utils import *
+import random
+class NRef(Ref):
+    cat = "word"
+class NDef(Def):
+    cat = "word"
+class WeightedOr(Field):
+    """A ``Field`` subclass that chooses one of the provided values at
+    random as the result of a call to the ``build()`` method. Takes an
+    odds array rather than just direct values."""
+    def __init__(self, *values, **kwargs):
+        '''
+        values is a list of tuples
+        '''
+        self.shortest_vals = None
+        vals=[x[0] for x in values]
+        self.values= list(map(maybe_binstr, vals))
+        self.weights = [x[1] for x in values]
+        #print(self.weights)
+        if abs(1.0 - sum(self.weights)) > 0.0001:
+            raise("Weights in WeightedOr don't sum to 1.0: {}".format(self.weights))
+        if "options" in kwargs and len(values) == 0:
+            self.values = list(map(maybe_binstr, kwargs["options"]))
+        self.rolling = kwargs.setdefault("rolling", False)
+    def build(self, pre=None, shortest=False):
+        """
+        :param list pre: The prerequisites list
+        :param bool shortest: Whether or not the shortest reference-chain (most minimal) 
+        version of the field should be generated.
+        """
+        #self.values=list(map(val, self.values))
+        #self.shortest_vals=list(map(val, self.values))
+        if pre is None:
+            pre = []
+        # self.shortest_vals will be set by the GramFuzzer and will
+        # contain a list of value options that have a minimal reference chain
+        if shortest and self.shortest_vals is not None:
+            return utils.val(random.choices(self.shortest_vals, self.weights)[0], pre, shortest=shortest)
+        else:
+            return utils.val(random.choices(self.values, self.weights)[0], pre, shortest=shortest)
+    #def build(self, pre=None, shortest=False):
+    #    s=self.helperbuild(pre=None, shortest=False).decode('utf-8')
+    #    start=s.find('[')+3
+    #    end=s.find(']')-1
+    #    return bytes(s[start:end], 'utf-8')
+NDef("a", "a string")
+NDef("B", NRef("a"))
+NDef("c", "c string")
+NDef("d", "d string")
+y=Or(NRef("a"), NRef("B"), NRef("c"))
+x=y=Or(NRef("a"), NRef("B"), NRef("c"))
+x=WeightedOr(
+    (NRef("c"), 0.25),
+    (NRef("B"), 0.25),
+    (NRef("a"), 0.25),
+    (NRef("r"), 0.25)
+)
+NDef("r", WeightedOr(
+    (NRef("r"), 0.25),
+    (And(
+        NRef("B"),
+        NRef("r")
+     ), 0.25),
+    (NRef("B"), 0.25),
+    (NRef("d"), 0.25)
+))
+#print(x)
+#print(x.build())
+assert(type(x.build())==bytes)
+#print(y)
+#print(y.build())
+assert(type(y.build())==bytes)
+
