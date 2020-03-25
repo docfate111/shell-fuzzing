a4a1f4ffcc535db75535a81e584ddbbe99f00bd8
Williams_Thelford <tdwilliamsiv@gmail.com>
2020-03-24 17:40:30 -0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import gramfuzz
-fuzzer = gramfuzz.GramFuzzer()
-fuzzer.debug = True
-fuzzer.load_grammar("words.py")
-code = list(fuzzer.gen(cat="program", num=10, max_recursion=15))
-# or like this if GRAMFUZZ_TOP_LEVEL_CAT="name" was defined in the
-# names_grammar.py file:
-#     names = fuzzer.gen(cat_group="names_grammar", num=10)
-print("\n".join([x.decode('utf-8') for x in code]))
