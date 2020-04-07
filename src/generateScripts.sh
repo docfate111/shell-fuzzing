0d4c92930417ca284e1d131b0f7fb78619ae1af7
docfate111 <tdwilliamsiv@gmail.com>
2020-04-07 10:01:58 -0700

    changes to dockerfile

--- src/generateScripts.sh
+++ src/generateScripts.sh
@@ -5,6 +5,5 @@ chmod 777 testscripts
 mv testscripts /var/www/tests
 for x in {1..15}
 do
-    mkdir /var/www/tests/testscripts
     python3 main.py >> '/var/www/tests/testscripts/script'$x'.sh'
 done
