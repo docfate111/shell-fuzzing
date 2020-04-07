13b617a6b219726c055a504b21984f366ece0500
docfate111 <tdwilliamsiv@gmail.com>
2020-04-07 11:51:26 -0700

    error in gramfuzz src

--- src/generateScripts.sh
+++ src/generateScripts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#pip3 install gramfuzz
-pip install gramfuzz
+pip3 install gramfuzz
+#pip install gramfuzz
 mkdir /home/tests/testscripts
 for x in {1..15}
 do
-    python main.py >> '/home/tests/testscripts/script'$x'.sh'
+    python3 main.py >> '/home/tests/testscripts/script'$x'.sh'
 done
