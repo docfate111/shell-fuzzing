f23e6ac8393b9b5da1714d1caf2d163600836bfe
docfate111 <tdwilliamsiv@gmail.com>
2020-04-07 11:48:01 -0700

    using python not python3 b/c bug still not fixed

--- src/generateScripts.sh
+++ src/generateScripts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-pip3 install gramfuzz
+#pip3 install gramfuzz
+pip install gramfuzz
 mkdir /home/tests/testscripts
 for x in {1..15}
 do
-    python3 main.py >> '/home/tests/testscripts/script'$x'.sh'
+    python main.py >> '/home/tests/testscripts/script'$x'.sh'
 done
