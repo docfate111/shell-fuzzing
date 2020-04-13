796917369448f776b8e60698f0ff028969dfb2ed
docfate111 <tdwilliamsiv@gmail.com>
2020-04-13 11:08:46 -0700

    switching from python3 to python

--- src/generateScripts.sh
+++ src/generateScripts.sh
@@ -4,5 +4,5 @@ pip install gramfuzz
 mkdir /home/tests/testscripts
 for x in {1..15}
 do
-    python3 main.py >> '/home/tests/testscripts/script'$x'.sh'
+    python main.py >> '/home/tests/testscripts/script'$x'.sh'
 done
