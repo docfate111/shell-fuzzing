886e5ed9c2fcf27a30f982256b0dbfa5fc85cc4e
docfate111 <tdwilliamsiv@gmail.com>
2020-04-21 20:38:30 -0700

    runs tests and gets info

--- src/scriptGeneration/generateScripts.sh
+++ src/scriptGeneration/generateScripts.sh
@@ -5,10 +5,10 @@ if [ "$#" -ne 1 ]; then
 fi
 pip install gramfuzz
 #python3-pip install gramfuzz
-mkdir /home/tests/testscripts
+mkdir -p ~/home/tests/testscripts
 #installs gramfuzz, makes directory to hold the test scripts,
 #and makes as many scripts as the argument provided
 for x in $(seq "$1")
 do
-    python main.py >> '/home/tests/testscripts/script'$x'.sh'
+    python main.py >> '~/home/tests/testscripts/script'$x'.sh'
 done
