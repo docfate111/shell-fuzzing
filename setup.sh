88db839ada1b8a90d5adc5676d0e07dd09b33ae2
docfate111 <tdwilliamsiv@gmail.com>
2020-05-09 17:29:40 -0700

    creating nonroot user

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build -t testingbox setup
-docker run -it testingbox:latest
+docker build -t shellfuzz setup
+#run but not as root so we can run afl-fuzz
+docker run -user newuser -it shellfuzz:latest
