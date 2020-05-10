e45158d2a693bbd0bd4c90756a716c5e1f407110
docfate111 <tdwilliamsiv@gmail.com>
2020-05-09 17:35:01 -0700

    fixing network issue

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t shellfuzz setup
+docker build --network=host -t shellfuzz setup
 #run but not as root so we can run afl-fuzz
 docker run -user newuser -it shellfuzz:latest
