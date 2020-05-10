cce978121816cf961faed7f21c5a0648074141e4
docfate111 <tdwilliamsiv@gmail.com>
2020-05-09 18:13:28 -0700

    changes made

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build --network=host -t shellfuzz setup
 #run but not as root so we can run afl-fuzz
-docker run -user newuser -it shellfuzz:latest
+docker run --user newuser -it shellfuzz:latest
