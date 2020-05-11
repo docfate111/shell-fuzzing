211b2e64921b0494c7cf501dc5aa494a89d72f5e
docfate111 <tdwilliamsiv@gmail.com>
2020-05-10 19:59:50 -0700

    afl-fuzz permissions?

--- src/scriptGeneration/main.py
+++ src/scriptGeneration/main.py
@@ -4,5 +4,5 @@ fuzzer = gramfuzz.GramFuzzer()
 fuzzer.debug = True
 fuzzer.load_grammar("specified.py")
 #what should flags be?
-code = list(fuzzer.gen(cat="start", num=random.randint(0, 100), max_recursion=random.randint(0, 50)))
+code = list(fuzzer.gen(cat="start", num=10, max_recursion=20))
 print("\n".join([x.decode('utf-8') for x in code]))
