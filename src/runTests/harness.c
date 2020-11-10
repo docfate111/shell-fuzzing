78f0161890443b1cdd868a3b4e6ec8380261e0e7
docfate111 <tdwilliamsiv@gmail.com>
2020-11-09 23:11:51 -0600

    changes to harness, idk why it isnt working

--- src/runTests/harness.c
+++ src/runTests/harness.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #define LSIZ 15
 #define RSIZ 15
 #define SIZE 300
@@ -22,14 +23,14 @@ int run_shell(char shellname[], char* filename){
     char* complete_path = (char*)malloc(36);
     strncat(complete_path, "~/smoosh-fuzz/shells/bin/", 25);
     strncat(complete_path, shellname, 11);
+    puts(complete_path);
     // Fork and fixup STDIN/STDOUT/STDERR.
     int id = fork();
     if(id==0){
         // close stdin/stdout/stderr
-        close(stdin);
-        close(stderr);
-        close(stdout);
-        char* complete_path = (char*)malloc(36);
+        // close(stdin);
+        // close(stderr);
+        // close(stdout);
         // open new file for stdin/stdout/stderr
         char* stdinfilename = strdup(shellname);
         strcat(stdinfilename, "stdin");
@@ -37,18 +38,27 @@ int run_shell(char shellname[], char* filename){
         strcat(stdoutfilename, "stdout");
         char* stderrfilename = strdup(shellname);
         strcat(stderrfilename, "stderr");
-        FILE* fstdin = fopen(stdinfilename, "w");
-        FILE* fstderr = fopen(stdoutfilename, "w");
-        FILE* fstdout = fopen(stderrfilename, "w");
+        // renumber file descriptors
+        FILE* fstdin = fopen(stdinfilename, "w+");
+        FILE* fstderr = fopen(stderrfilename, "w+");
+        FILE* fstdout = fopen(stdoutfilename, "w+");
+        if(dup2(fileno(fstdin), STDIN_FILENO)==-1){
+            perror("Error");
+        }
+        if(dup2(fileno(fstdout), STDOUT_FILENO)==-1){
+             perror("Error");
+        }
+        if(dup2(fileno(fstderr), STDERR_FILENO)==-1){
+            perror("Error");
+        }
         char* argv[1];
         argv[0] = filename;
-        execve(complete_path, argv, NULL);
+        execve(complete_path, argv, 0);
     }
-    // Save STDOUT and STDERR to separate files.
-    // Call execve on the given shell with the provided file as an argument.
     // Call wait to get the exit code. Record the exit code in a file.
     // The outer loop that calls fork all of those times should call 
     // wait an equal number of times (or until wait gives up). Then do the comparisons.
+    return 0;
 }
 int main(int argc, char** argv) {
     if(argc != 2){
@@ -62,7 +72,7 @@ int main(int argc, char** argv) {
     }
     char list_of_shells[RSIZ][LSIZ];
     int num_of_shells = get_installed_shells(list_of_shells);
-    for(size_t i=0; i<num_of_shells; i++){
+    for(int i=0; i<num_of_shells; i++){
         char* file_to_run = (char*)malloc(12);
         strncpy(file_to_run, argv[1], 11);
         run_shell(list_of_shells[i], file_to_run);
