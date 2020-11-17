b6d17ad6f91605925037982f4758dc984765399d
docfate111 <tdwilliamsiv@gmail.com>
2020-11-16 22:21:41 -0600

    fixing bug in run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 echo 'Make sure smoosh-fuzz is home directory'
-#./provision.sh
+mkdir -p ~/smoosh-fuzz/seeds
 cd ~/smoosh-fuzz/shells
 make all
 cd ~/smoosh-fuzz
 # make scripts and put them in seeds
-src/scriptGeneration/generateScripts.sh
-echo 'TODO: change checkInstall.py to write all files in smoosh-fuzz/shells/bin into file labelled "installed_shells"' 
+src/scriptGeneration/generateScripts.sh 30
 cd  src/runTests
 # get list of shells installed in smoosh-fuzz/shells/bin
 ./checkInstall.sh
